a0031279f895994e5d47c8be99effab34488965e
Clency Tabe <clencytabe@gmail.com>
2023-02-02 22:50:59 -0700

    adjust

--- src/RASTER.C
+++ src/RASTER.C
@@ -5,7 +5,7 @@
 
 void plot_pixel(char *base, int x, int y)
 {
-	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) 
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
 	{
 		*(base + y * 80 + x) = 0xff;
 	}
@@ -14,17 +14,98 @@ void plot_pixel(char *base, int x, int y)
 void plot_vertical_line(char *base, int x1, int y1, int y2)
 {
 	int i;
-	
-	if (x1 >= 0 && x1 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT && y2 >= 0 && y2 < SCREEN_HEIGHT) 
+
+	if (x1 >= 0 && x1 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT && y2 >= 0 && y2 < SCREEN_HEIGHT)
 	{
 		if (y1 >= y2)
 		{
-			for(i = x1; i > y2; i--)
+			for (i = 0; i > y2; i--)
 			{
 				*(base + y1 * 80 + x1) = 0xff;
-				y2 --;
+				y1--;
 			}
 		}
-		
+		else
+		{
+			for (i = 0; i < y2; i++)
+			{
+				*(base + y1 * 80 + x1) = 0xff;
+				y1++;
+			}
+		}
+	}else
+	{
+		return;
+	}
+}
+
+void plot_horizontal_line(char *base, int x1, int x2, int y1)
+{
+	int i;
+
+	if (x1 >= 0 && x1 < SCREEN_WIDTH && x2 >= 0 && x2 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT)
+	{
+		if (x1 >= x2)
+		{
+			for (i = 0; i > x2; i--)
+			{
+				*(base + y1 * 80 + x1) = 0xff;
+				x1--;
+			}
+		}
+		else
+		{
+			for (i = 0; i < x2; i++)
+			{
+				*(base + y1 * 80 + x1) = 0xff;
+				x1++;
+			}
+		}
+	}
+	else
+	{
+		return;
+	}
+}
+
+void plot_bitmap_8(UINT8 *base, int x, int y, const UINT8 *bitmap, unsigned int height)
+{
+	int i;
+
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+	{
+		for (i = 0; i < height; i++)
+		{
+			*(base + y * 80 + x) = bitmap[i];
+			y++;
+		}
+	}
+}
+
+void plot_bitmap_16(UINT16 *base, int x, int y, const UINT16 *bitmap, unsigned int height)
+{
+	int i;
+
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+	{
+		for (i = 0; i < height; i++)
+		{
+			*(base + y * 80 + x) = bitmap[i];
+			y++;
+		}
+	}
+}
+
+void plot_bitmap_32(UINT32 *base, int x, int y, const UINT32 *bitmap, unsigned int height)
+{
+	int i;
+
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+	{
+		for (i = 0; i < height; i++)
+		{
+			*(base + y * 80 + x) = bitmap[i];
+			y++;
+		}
 	}
 }
