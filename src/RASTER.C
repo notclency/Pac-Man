ef929fa1ba54ee1222739ceca8f4fa66ae6de128
Clency Tabe <clencytabe@gmail.com>
2023-02-05 11:15:54 -0700

    updated raster functions

--- src/RASTER.C
+++ src/RASTER.C
@@ -1,61 +1,104 @@
-#include "raster.h"
+#include "RASTER.H"
 
-#define SCREEN_WIDTH 640
+#define SCREEN_WIDTH 640 /* 80 working in bytes */
 #define SCREEN_HEIGHT 400
 
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x: x coordinate of the pixel
+		y: y coordinate of the pixel
+
+	description:
+		plots a pixel at the specified coordinates
+
+	returns:
+		none
+*/
 void plot_pixel(char *base, int x, int y)
 {
 	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
 	{
 		*(base + y * 80 + x) = 0xff;
 	}
+	else
+	{
+		return;
+	}
 }
 
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x1: x coordinate of the first point
+		y1: y coordinate of the first point
+		y2: y coordinate of the second point / end coordinate of the line
+
+	description:
+		plots a vertical line from the first point to the second point
+
+	returns:
+		none
+*/
 void plot_vertical_line(char *base, int x1, int y1, int y2)
 {
 	int i;
 
 	if (x1 >= 0 && x1 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT && y2 >= 0 && y2 < SCREEN_HEIGHT)
 	{
-		if (y1 >= y2)
+		if (y2 <= y1)
 		{
-			for (i = 0; i > y2; i--)
+			while (y2 <= y1)
 			{
-				*(base + y1 * 80 + x1) = 0xff;
-				y1--;
+				*(base + y2 * 80 + x1) = 0xff;
+				y2++;
 			}
 		}
 		else
 		{
-			for (i = 0; i < y2; i++)
+			while (y1 <= y2)
 			{
 				*(base + y1 * 80 + x1) = 0xff;
 				y1++;
 			}
 		}
-	}else
+	}
+	else
 	{
 		return;
 	}
 }
 
-void plot_horizontal_line(char *base, int x1, int x2, int y1)
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x1: x coordinate of the first point
+		y1: y coordinate of the first point
+		x2: x coordinate of the second point / end coordinate of the line
+
+	description:
+		plots a horizontal line from the first point to the second point
+
+	returns:
+		none
+*/
+void plot_horizontal_line(char *base, int x1, int y1, int x2)
 {
 	int i;
 
 	if (x1 >= 0 && x1 < SCREEN_WIDTH && x2 >= 0 && x2 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT)
 	{
-		if (x1 >= x2)
+		if (x2 <= x1)
 		{
-			for (i = 0; i > x2; i--)
+			while (x2 <= x1)
 			{
-				*(base + y1 * 80 + x1) = 0xff;
-				x1--;
+				*(base + y1 * 80 + x2) = 0xff;
+				x2++;
 			}
 		}
 		else
 		{
-			for (i = 0; i < x2; i++)
+			while (x1 <= x2)
 			{
 				*(base + y1 * 80 + x1) = 0xff;
 				x1++;
@@ -110,39 +153,151 @@ void plot_bitmap_32(UINT32 *base, int x, int y, const UINT32 *bitmap, unsigned i
 	}
 }
 
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x1: x coordinate of the first point
+		y1: y coordinate of the first point
+		x2: x coordinate of the second point
+		y2: y coordinate of the second point
 
+	description:
+		plots a rectangle with the specified coordinates
+
+	returns:
+		none
+*/
 void plot_rectangle(char *base, int x1, int y1, int x2, int y2)
 {
-	/* Do something */
-}
-
-void plot_square(char *base, int x, int y, int size)
-{
-	/* Do something */
-}
-
-void clear_screen(char *base)
-{
-	int x;
-
-	for (x = 0; x < SCREEN_WIDTH * SCREEN_HEIGHT; x++)
+	if (x1 >= 0 && x1 < SCREEN_WIDTH && x2 >= 0 && x2 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT && y2 >= 0 && y2 < SCREEN_HEIGHT)
 	{
-		*(base + x) = 0x00;
+		plot_horizontal_line(base, x1, y1, x2);
+		plot_horizontal_line(base, x1, y2, x2);
+		plot_vertical_line(base, x1, y1, y2);
+		plot_vertical_line(base, x2, y1, y2);
+	}
+	else
+	{
+		return;
 	}
 }
 
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x: x coordinate of the center of the square
+		y: y coordinate of the center of the square
+		size: size of the square
+
+	description:
+		plots a square with the specified size
+
+	returns:
+		none
+*/
+void plot_square(char *base, int x, int y, int size)
+{
+	int i;
+	int j;
+
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+	{
+		plot_horizontal_line(base, x, y, x + size);
+		plot_vertical_line(base, x, y, y + size);
+		plot_vertical_line(base, x + size, y, y + size);
+		plot_horizontal_line(base, x, y + size, x + size);
+	}
+	else
+	{
+		return;
+	}
+}
+
+/*
+	parameters:
+		base: pointer to the base address of the screen
+
+	description:
+		clears the screen
+
+	returns:
+		none
+*/
+void clear_screen(char *base)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 80; i++)
+	{
+		for (j = 0; j < 400; j++)
+		{
+			clear_pixel(base, i, j);
+		}
+	}
+}
+
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x: x coordinate of the pixel
+		y: y coordinate of the pixel
+
+	description:
+		clears a pixel at the specified coordinates
+
+	returns:
+		none
+*/
+void clear_pixel(char *base, int x, int y)
+{
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+	{
+		*(base + y * 80 + x) = 0x00;
+	}
+}
+
+/*
+	parameters:
+		base: pointer to the base address of the screen
+		x1: x coordinate of the first point
+		y1: y coordinate of the first point
+		x2: x coordinate of the second point
+		y2: y coordinate of the second point
+
+	description:
+		clears a region of the screen
+
+	returns:
+		none
+*/
 void clear_region(char *base, int x1, int y1, int x2, int y2)
 {
-	int x, y;
+	int i;
+	int j;
 
 	if (x1 >= 0 && x1 < SCREEN_WIDTH && x2 >= 0 && x2 < SCREEN_WIDTH && y1 >= 0 && y1 < SCREEN_HEIGHT && y2 >= 0 && y2 < SCREEN_HEIGHT)
 	{
-		for (y = y1; y < y2; y++)
+		for (i = x1; i <= x2; i++)
 		{
-			for (x = x1; x < x2; x++)
+			for (j = y1; j <= y2; j++)
 			{
-				*(base + x) = 0x00;
+				clear_pixel(base, i, j);
 			}
+		}
+	}
+}
+
+void black_screen(char *base)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 80; i++)
+	{
+		for (j = 0; j < 400; j++)
+		{
+			plot_pixel(base, i, j);
 		}
 	}
 }
--- src/TST_RAST.C
+++ src/TST_RAST.C
@@ -1,12 +1,69 @@
 #include <osbind.h>
-#include "raster.h"
+#include <stdio.h>
+#include "RASTER.H"
+
+UINT16 pacman[16] = {
+		0xFFFF, 0xFC3F, 0xF00F, 0xE007, 
+		0xC003, 0xC003, 0xF801, 0xFC01, 
+		0xFE01, 0xFC01, 0xF803, 0xC003, 
+		0xE007, 0xF00F, 0xFC3F, 0xFFFF 
+};
+
+UINT16 invader_bitmap[16] = 
+{
+    0x0000,
+    0x0810,
+    0x0810,
+    0x0420,
+    0x0240,
+    0x1FF8,
+    0x2004,
+    0x4662,
+    0x4002,
+    0x43C2,
+    0x2424,
+    0x1008,
+    0x0FF0,
+    /*last 3 elements*/
+    0x0240,
+    0x0E70,
+    0x0000
+};
 
 int main()
 {
-	void *base = Physbase();
+	/*int i;
+	/*int j;
 
-	plot_something_1(base, 100, 100);
-	plot_something_2(base, 200, 200);
+	/*void *base = Physbase();
+
+	/*clear_screen(base);
+
+	/*plot_pixel(base, 79, 79);
+	/*plot_pixel(base, 79, 330);
+
+	/*plot_vertical_line(base, 75, 330, 79); /* x1,y2 < y1 */
+	/*plot_vertical_line(base, 77, 79, 330); /* x1,y1 < y2 */
+
+	/*plot_horizontal_line(base, 30, 79, 60); /* x2 > x1 */
+	/*plot_horizontal_line(base, 60, 85, 30); /* x2 < x1 */
+
+	/*clear_region(base, 30, 79, 60, 79);	 /* clear top line */
+	/*clear_region(base, 77, 79, 77, 330); /* clear right line */
+
+	/*plot_square(base, 1, 1, 50);
+	/*plot_rectangle(base, 1, 100, 51, 200);*/
+
+	void *base = Physbase();
+	UINT32 *base32 = (UINT32 *)Physbase();
+	UINT16 *base16 = (UINT16 *)Physbase();
+
+	black_screen(base);
+
+	plot_bitmap_16(base16, 20, 10, pacman, 16);
+	plot_bitmap_16(base16, 10, 10, invader_bitmap, 16);
+	plot_bitmap_16(base16, 11, 10, invader_bitmap, 16);
+
 
 	return 0;
 }
